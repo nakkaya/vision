322765ae63ded97575ff84e678f1edae1d6a2074
Nurullah Akkaya <nurullah@nakkaya.com>
2011-01-20 04:17:32 +0200

    add segmentation example code

---
+++ resources/examples/segmentation.clj
@@ -0,0 +1,18 @@
+(ns segmentation
+  (:use [vision core graphics] :reload-all))
+
+(let [image (load-image "resources/samples/plate.jpeg" :color)
+      processed (--> (convert-color image :bgr-gray)
+                     (smooth :gaussian 3 3 0 0)
+                     (threshold 30 255 :binary-inv)
+                     (erode 1))
+      cs (with-contours [c [processed :external :chain-approx-simple [0 0]]]
+           (bounding-rects c))]
+
+  (doseq [[x y w h] cs] 
+    (rectangle image x y w h java.awt.Color/red 1))
+  
+  (show-image :result image)
+
+  (release-image image)
+  (release-image processed))
