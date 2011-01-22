0f38572025ca3a864019705057e4859ea17de5cb
Nurullah Akkaya <nurullah@nakkaya.com>
2011-01-22 09:12:32 +0200

    remove graphics/util .clj

--- src/vision/util.clj
+++
@@ -1,9 +0,0 @@
-(ns vision.util)
-
-(defn map-int [x in-min in-max out-min out-max]
-  (+ (/ (* (- x in-min) (- out-max out-min)) (- in-max in-min)) out-min))
-
-(defn in-range? [x [a b]]
-  (if (and (>= x a)
-           (<= x b))
-    true false))
