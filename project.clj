3753186657805a14967810d784149c2ea1c98f85
Nurullah Akkaya <nurullah@nakkaya.com>
2011-06-05 19:40:41 +0300

    add dep lein-clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.nakkaya/jna "3.2.7"]]
-  :jvm-opts ["-Djna.library.path=resources/lib/"])
+  :jvm-opts ["-Djna.library.path=resources/lib/"]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
