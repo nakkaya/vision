c753b9b4c123f89e11b08e285a2eca43a5392ef1
Nurullah Akkaya <nurullah@nakkaya.com>
2011-10-23 01:21:02 +0300

    remove comment

--- resources/lib/vision.c
+++ resources/lib/vision.c
@@ -1083,10 +1083,6 @@ void contours(void *i, void *c, int r1, int g1, int b1, int r2, int g2, int b2, 
   cvDrawContours(img, contours, cvScalar(b1,g1,r1,0), cvScalar(b2,g2,r2,0), level, thickness, 8, cvPoint(0,0));
 }
 
-//
-// Begin Daniel Cousineau Patch Work
-//
-
 void* flip_image(void* i, int mode){
   IplImage* image = (IplImage*)i;
   IplImage* flipped = cvCreateImage(cvGetSize((IplImage*)image), image->depth, image->nChannels);;
