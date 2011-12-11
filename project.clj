ea5a435f3fa6e48bf7dbc20d2b61589c43648ac9
Nurullah Akkaya <nurullah@nakkaya.com>
2011-12-11 17:08:20 +0200

    remove lein-clojars plugin

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.nakkaya/jna "3.2.7"]]
-  :jvm-opts ["-Djna.library.path=resources/lib/"]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :jvm-opts ["-Djna.library.path=resources/lib/"])
